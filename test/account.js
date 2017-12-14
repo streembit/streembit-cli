d9f8aeb55a8b7ae2438254a9d50f30f8db6a292e
tanvirrb <tanvir.rb@outlook.com>
2017-12-14 20:31:37 +0600

    test accountname

--- test/account.js
+++ test/account.js
@@ -3,6 +3,7 @@ const expect = require("chai").expect;
 const util = require("util");
 const res = require('../resolvedir');
 const Account = require('libs/account');
+const account_data = require('./account_data.json');
 //const peerutils = require("libs/peernet/peerutils");
 //const stutils = require("libs/utils");
 //const constants = require("libs/constants");
@@ -25,4 +26,21 @@ describe("Account module test lib/account", function () {
             assert.equal(account.m_accountname, null);
         });
     });
+
+    describe("Account name", function () {
+
+        it("Should set a value to m_accountname", function () {
+            account.accountname = account_data.name;
+            let account_name = account.accountname;
+
+            assert.equal(account_name, account_data.name);
+        });
+
+        it("Should neither null nor undefined", function () {
+            account.accountname = account_data.name;
+            let account_name = account.accountname;
+
+            assert.exists(account_name);
+        });
+    })
 });
