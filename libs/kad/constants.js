c7a70d559ac1041d1dff07100f43f34117a4110f
zsoltpardi <zsolt.pardi@gmail.com>
2017-12-20 18:24:29 +0000

    Roll back the kad/constants changes.

--- libs/kad/constants.js
+++ libs/kad/constants.js
@@ -34,7 +34,6 @@ Copyright (C) 2016 The Streembit software development team
 'use strict';
 
 var ms = require('ms');
-var config = require('config');
 
 module.exports = {
 
@@ -46,21 +45,21 @@ module.exports = {
 
     /** @constant {Number} K - Number of contacts held in a bucket */
     K: 20,
-    
+
     /** @constant {Number} T_REFRESH - Interval for performing router refresh */
-    T_REFRESH: ms(config.limits.refresh + 's'),
+    T_REFRESH: ms('3600s'),
 
     /** @constant {Number} T_REPLICATE - Interval for replicating local data */
-    T_REPLICATE: ms(config.limits.replicate + 's'),
+    T_REPLICATE: ms('3600s'),
 
     /** @constant {Number} T_REPUBLISH - Interval for republishing data */
-    T_REPUBLISH: ms(config.limits.refresh + 's'),
+    T_REPUBLISH: ms('86400s'),
 
     /** @constant {Number} T_EXPIRE - Interval for expiring local data entries */
-    T_EXPIRE: ms(config.limits.republish + 's'),
+    T_EXPIRE: ms('86405s'),
 
     /** @constant {Number} T_RESPONSETIMEOUT - Time to wait for RPC response */
-    T_RESPONSETIMEOUT: ms(config.limits.timeout + 's'),
+    T_RESPONSETIMEOUT: ms('5s'),
 
     /** @constant {Array} MESSAGE_TYPES - Allowed RPC methods */
     MESSAGE_TYPES: [
