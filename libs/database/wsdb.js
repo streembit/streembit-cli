aa783eaf44232a5656751c61f4e71b272aa05405
zsoltpardi <zsolt.pardi@gmail.com>
2018-01-13 16:00:17 +0000

    Changed author details.

--- libs/database/wsdb.js
+++ libs/database/wsdb.js
@@ -13,7 +13,7 @@ You should have received a copy of the GNU General Public License along with Str
 If not, see http://www.gnu.org/licenses/.
 
 -------------------------------------------------------------------------------------------------------------------------
-Author: Tibor Z Pardi
+Author: Streembit team
 Copyright (C) 2017 The Streembit software development team
 -------------------------------------------------------------------------------------------------------------------------
 
