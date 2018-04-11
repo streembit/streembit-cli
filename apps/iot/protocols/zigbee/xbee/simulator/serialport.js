ced4cf49845df4333d2531931c984c96ddfadfec
AlDoo <alex.doo.gm@gmail.com>
2018-04-11 16:23:00 +0300

    xbee simulator MockBinding

--- apps/iot/protocols/zigbee/xbee/simulator/serialport.js
+++ apps/iot/protocols/zigbee/xbee/simulator/serialport.js
@@ -1,15 +1,16 @@
+const GenuineSerialPort = require('serialport/test');
+const MockBinding = GenuineSerialPort.Binding;
+
 class SerialPort {
-    constructor() {}
-
-    write() {
-
+    constructor(port, options, errHandler) {
+        // Create a port and enable the echo and recording.
+        try {
+            MockBinding.createPort(port, Object.create({ echo: true, record: true }, options));
+            this.port = new GenuineSerialPort(port);
+        } catch (err) {
+            errHandler(err);
+        }
     }
-
-    read() {
-
-    }
-
-    //
 }
 
 module.exports = SerialPort;
