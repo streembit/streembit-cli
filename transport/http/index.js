0d21a9de1ac2faa484c1a48536ed3cb63c903fff
zsoltpardi <tibor@zovolt.com>
2018-09-13 23:56:29 +0100

    Added console log.

--- transport/http/index.js
+++ transport/http/index.js
@@ -107,14 +107,16 @@ class HTTPTransport {
 
             if (!payload) {
                 // return a success status for the GET request
-                // GET request is only fro health check
+                // GET request is only for health check
                 if (req.method === 'GET') {
+                    console.log('health check GET');
                     res.statusCode = 200;
                     return res.end();
                 }
-
-                // for POST method the payload must exists
-                throw new Error("invalid payload");
+                else {
+                    // for POST method the payload must exists
+                    throw new Error("invalid payload");
+                }
             }
 
             var message;
@@ -136,11 +138,11 @@ class HTTPTransport {
         catch (err) {
             // bad request
             try {
+                logger.error("HTTPTransport routemsg exception handler error: " + err.message);
                 res.statusCode = 405;
                 res.end();
             }
-            catch (e){ }
-            logger.error("HTTPTransport routemsg error2: " + err.message);
+            catch (e){ }            
         }
     }
 
