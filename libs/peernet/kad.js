ae339cc5200956508d188a7c5e109922e29389ba
AlDoo <alex.doo.gm@gmail.com>
2018-09-13 16:24:57 +0300

    ssl certs removed from peernet/kad

--- libs/peernet/kad.js
+++ libs/peernet/kad.js
@@ -173,12 +173,6 @@ class KadHandler {
             seeds: options.seeds
         };
 
-        if (config.transport.ssl) {
-            init_options.contact.cert = fs.readFileSync(config.transport.cert);
-            init_options.contact.key = fs.readFileSync(config.transport.key);
-            init_options.contact.ca = fs.readFileSync(config.transport.ca);
-        }
-
         var node = new kad.Node();
         node.init(init_options, (err, peer, peercount) => {
             if (err) {
