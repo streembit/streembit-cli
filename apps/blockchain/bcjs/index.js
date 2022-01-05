71451af46ed3489d3bb56a95f92d69e0e91cd98a
mansuryan <tigran.mansuryan92@gmail.com>
2022-01-05 19:49:27 +0400

    Almost finish blockchain integration

---
+++ apps/blockchain/bcjs/index.js
@@ -0,0 +1,20 @@
+
+import { Block } from './block.js';
+import * as BcKey from './bckey.js';
+import * as ECSignature from './ecsignature.js';
+import * as HDNode from './hdnode.js';
+import * as Transaction from './transaction.js';
+import * as address from './address.js';
+import * as crypto from './crypto.js';
+import * as networks from './networks.js';
+
+export {
+  Block,
+  BcKey,
+  ECSignature,
+  HDNode,
+  Transaction,
+  address,
+  crypto,
+  networks
+}
